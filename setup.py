3200d10bb5cb87e72cd76bd766846279a9df67e3
Diogo <86491085+Setserp13@users.noreply.github.com>
2023-03-21 23:59:23 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='MyLab',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Diogo Garcia',
+      author_email='diogogpsantos@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
