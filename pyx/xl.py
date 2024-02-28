325f7d9c49e5a9abf6fde73c798a7f82e737447b
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-02-28 08:19:08 -0300

    Update xl.py

--- pyx/xl.py
+++ pyx/xl.py
@@ -372,7 +372,7 @@ def max_w(matrices):
 	return mu.map_columns(matrices, lambda x, i: reduce(au.map(x, lambda x, i: x.max_column), lambda total, currentValue: max(total, currentValue)))
 
 def max_h(matrices):
-	return au.map(matrices, lambda x, i: reduce(au.map(x, lambda x, i: x.max_row), lambda total, currentValue: max(total, currentValue)))
+	return au.map(matrices, lambda x, i: reduce([y.max_row for y in x], lambda total, currentValue: max(total, currentValue)))
 
 
 
