cd62afa72d0564da870b5b34c18405778d643ff5
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-03-22 12:56:27 -0300

    Add files via upload

---
+++ pyx/requestsx.py
@@ -0,0 +1,18 @@
+import os
+import pyx.osx as osx
+import requests
+from urllib.parse import urlparse
+
+def download_image(url, filename=None, dir=None):
+	try:
+		response = requests.get(url)
+		if response.status_code == 200:
+			urlpath = urlparse(url).path
+			basename = f'{osx.filename(urlpath) if filename == None else filename}{osx.ext(urlpath)}'
+			file = osx.to_distinct(basename if dir == None else os.path.join(dir, basename))
+			with open(file, mode='wb') as f: f.write(response.content)
+			print("Image downloaded successfully")
+		else:
+			print(f"Failed to download image. Status code: {response.status_code}")
+	except Exception as e:
+		print(f"An error occurred: {e}")
