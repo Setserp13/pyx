92961ce30baf9d8bd4b3816982da2d4c0c339bf3
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-04-02 19:20:50 -0300

    Update pydubx.py

--- pyx/pydubx.py
+++ pyx/pydubx.py
@@ -1,3 +1,4 @@
+import functools
 from pydub import AudioSegment
 
-AudioSegment.merge = lambda ls: reduce(lambda x, y: x+y, ls)#, AudioSegment.silent(0))
+AudioSegment.merge = lambda ls: functools.reduce(lambda x, y: x+y, ls)#, AudioSegment.silent(0))
