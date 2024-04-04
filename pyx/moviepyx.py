40eb7d65fa00bdd8b775fda98b6ac9852cf71050
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-04-04 15:41:10 -0300

    Update moviepyx.py

--- pyx/moviepyx.py
+++ pyx/moviepyx.py
@@ -1,9 +1,17 @@
 import moviepy.editor as mp
 
 def merge_av(audio_path, video_path, output_path):
+	output_path = osx.to_distinct(output_path)
 	audio = mp.AudioFileClip(audio_path)
 	video = mp.VideoFileClip(video_path)
 	video.set_audio(audio).write_videofile(output_path, fps=video.fps)
+
+def set_video_duration(video_path, duration, speed_based=False, output_path=None):
+	output_path = osx.to_distinct(video_path) if output_path == None else output_path
+	video = VideoFileClip(video_path)
+	video.fx(vfx.speedx, duration if speed_based else video.duration / duration).write_videofile(output_path)
+	#return video
+	#return output_path
 
 
 
