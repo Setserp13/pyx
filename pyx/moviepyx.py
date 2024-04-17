502935edb30bcd4b65661a52baad52c867444ad6
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-04-17 08:11:38 -0300

    Update moviepyx.py

--- pyx/moviepyx.py
+++ pyx/moviepyx.py
@@ -2,7 +2,7 @@ import moviepy.editor as mp
 import pyx.osx as osx
 
 def merge_av(audio_path, video_path, output_path):
-	output_path = osx.to_distinct(output_path)
+	output_path = osx.to_distinct(video_path) if output_path == None else output_path
 	audio = mp.AudioFileClip(audio_path)
 	video = mp.VideoFileClip(video_path)
 	video.set_audio(audio).write_videofile(output_path, fps=video.fps)
