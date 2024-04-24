e45bdefb78689cb708ca7188651f91bf8cc16dc7
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-04-24 15:49:46 -0300

    Update cv2x.py

--- pyx/cv2x.py
+++ pyx/cv2x.py
@@ -6,22 +6,22 @@ def fps(self): return self.get(cv2.CAP_PROP_FPS)
 
 def frame_count(self): return self.get(cv2.CAP_PROP_FRAME_COUNT)
 
-def size(self): return (self.height(), self.width())
+def size(self): return (height(self), width(self))
 
 def width(self): return int(self.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-def duration(self): return self.frame_count() / self.fps()
+def duration(self): return frame_count(self) / fps(self)
 
-def frame_index(self, t): return int(self.fps() * t)
+def frame_index(self, t): return int(fps(self) * t)
 
 def get_frame(self, index):
-	self.set(cv2.CAP_PROP_POS_FRAMES, min(index, self.frame_count() - 1))
+	self.set(cv2.CAP_PROP_POS_FRAMES, min(index, frame_count(self) - 1))
 	retval, image = self.read()
 	return image
 
-cv2.VideoCapture.height = height
+"""cv2.VideoCapture.height = height
 cv2.VideoCapture.fps = fps
 cv2.VideoCapture.frame_count = frame_count
 cv2.VideoCapture.size = size
 cv2.VideoCapture.width = width
-cv2.VideoCapture.duration = duration
+cv2.VideoCapture.duration = duration"""
