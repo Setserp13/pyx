2e436f4f7cc16490d0c872725da9dc2c838e6231
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-05-01 12:22:23 -0300

    Update PILx.py

--- pyx/PILx.py
+++ pyx/PILx.py
@@ -1,6 +1,6 @@
 from PIL import ImageFont
 
-def getsize(lines, leading, font, font_size):
+def getsize(lines, font, font_size, leading):
 	font = ImageFont.truetype(font, font_size)
 	width = max(font.getsize(line)[0] for line in lines)
 	height = sum(font.getsize(line)[1] for line in lines) + leading * (len(lines) - 1)
@@ -32,19 +32,19 @@ def wrap(line, width, font, font_size):
 				result[-1] += space + word
 	return result#'\n'.join(result)
 
-def truncate(lines, height, leading, font, font_size):
+def truncate(lines, height, font, font_size, leading):
 	for i in range(len(lines)):
-		if getsize(lines[:i+1], leading, font, font_size)[1] > height:
+		if getsize(lines[:i+1], font, font_size, leading)[1] > height:
 			return lines[:i], lines[i:]
 	return lines, []
 
-def best_fit(line, size, leading, font, max_font_size=300):
+def best_fit(line, size, font, leading, max_font_size=300):
 	font_size = max_font_size
 	lines = [line]
 	for i in range(max_font_size, 0, -1): #from max_font_size to 1
 		font_size = i
 		lines = wrap(line, size[0], font, font_size)
-		text_size = getsize(lines, leading, font, font_size)
+		text_size = getsize(lines, font, font_size, leading)
 		if text_size[1] <= size[1]:
 			break
 	return lines, font_size
