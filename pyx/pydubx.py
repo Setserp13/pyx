d2119589bf2125a762ccb313b4f25459c89548c7
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-06-13 12:40:06 -0300

    Update pydubx.py

--- pyx/pydubx.py
+++ pyx/pydubx.py
@@ -3,6 +3,6 @@ from pydub import AudioSegment
 
 AudioSegment.merge = lambda ls: functools.reduce(lambda x, y: x+y, ls)#, AudioSegment.silent(0))
 
-def overlay(ls): return functools.reduce(lambda a, b: a.overlay(b) if len(a) > len(b) else b.overlay(a), ls)
+def overlay(ls): return functools.reduce(lambda a, b: a.overlay(b) if len(a) >= len(b) else b.overlay(a), ls)
 
 def open_all(ls): return [AudioSegment.from_file(x) for x in ls]
