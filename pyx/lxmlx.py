0891062b2a3e84667967caf9fd21ea6a6b6dcc53
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-06-21 21:46:08 -0300

    Update lxmlx.py

--- pyx/lxmlx.py
+++ pyx/lxmlx.py
@@ -4,3 +4,18 @@ def create_child(parent, tag, **kwargs):
 	result = etree.Element(tag, **kwargs)
 	parent.append(result)
 	return result
+
+def find(self, match, iter=etree._Element.iter): #find descendant by default
+	for x in iter(self):
+		if match(x):
+			return x
+	return None
+
+def find_ancestor(self, match, dflt_value=None):
+	parent = self.getparent()
+	while parent is not None:
+		#print(parent.tag)
+		if match(parent):
+			return parent
+		parent = parent.getparent()
+	return dflt_value
