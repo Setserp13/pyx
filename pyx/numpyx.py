e3ab5b49b43a7b45210295e361817f5194cd9fd4
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-06-28 00:07:03 -0300

    Update numpyx.py

--- pyx/numpyx.py
+++ pyx/numpyx.py
@@ -51,15 +51,14 @@ class rect:
 	def aabb(a, b):
 		return rect.min_max(np.minimum(a.min, b.min), np.maximum(a.max, b.max))
 
-	def delta(a, b):
+	def distances(a, b):
 		return [max(a.min[i] - b.max[i], b.min[i] - a.max[i], 0) for i in range(len(a.min))]
 
-	def chebyshev_distance(a, b): return max(delta(a, b))
+	def chebyshev_distance(a, b): return max(distances(a, b))
 	
-	def distance(a, b): #euclidean distance
-		return sqrt(sum(x ** 2 for x in delta(a, b)))
+	def euclidean_distance(a, b): return sqrt(sum(x ** 2 for x in distances(a, b)))
 
-	def manhattan_distance(a, b): return sum(delta(a, b))
+	def manhattan_distance(a, b): return sum(distances(a, b))
 
 class grid:
 	def __init__(self, cell_size, offset=None, cell_gap=None):
