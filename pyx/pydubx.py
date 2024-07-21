4448128cc431a9cad7cf65701fcdeafab883ac51
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-07-21 01:14:10 -0300

    Update pydubx.py

--- pyx/pydubx.py
+++ pyx/pydubx.py
@@ -1,8 +1,27 @@
 import functools
-from pydub import AudioSegment
+from pydub import AudioSegment, silence
 
-AudioSegment.merge = lambda ls: functools.reduce(lambda x, y: x+y, ls)#, AudioSegment.silent(0))
+#AudioSegment.merge = lambda ls: functools.reduce(lambda x, y: x+y, ls)#, AudioSegment.silent(0))
+def concat(ls): return functools.reduce(lambda x, y: x+y, ls)#, AudioSegment.silent(0))
 
 def overlay(ls): return functools.reduce(lambda a, b: a.overlay(b) if len(a) >= len(b) else b.overlay(a), ls)
 
 def open_all(ls): return [AudioSegment.from_file(x) for x in ls]
+
+def remove_silence(audio, silence_threshold=-60, min_silence_len=50):
+    return concat(split_on_silence(audio, silence_threshold=silence_threshold, min_silence_len=min_silence_len))
+
+def strip_audio(audio, silence_threshold=-60, min_silence_len=50):#-40):
+    silent_ranges = silence.detect_silence(audio, silence_thresh=silence_threshold, min_silence_len=min_silence_len)
+    #print(silent_ranges)
+    if silent_ranges:
+        start_trim = silent_ranges[0][1] if silent_ranges[0][0] == 0 else 0
+        end_trim = silent_ranges[-1][0] if silent_ranges[-1][1] == len(audio) else len(audio)
+    else:
+        start_trim = 0
+        end_trim = len(audio)
+    return audio[start_trim:end_trim]
+
+def split_on_silence(audio, silence_threshold=-30, min_silence_len=500):
+	nonsilent_ranges = silence.detect_nonsilent(audio, silence_thresh=silence_threshold, min_silence_len=min_silence_len)
+	return [audio[x[0]:x[1] for x in nonsilent_ranges]
