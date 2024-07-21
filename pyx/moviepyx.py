5641505be3f20073318951270fb5129292847259
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-07-21 13:10:35 -0300

    Update moviepyx.py

--- pyx/moviepyx.py
+++ pyx/moviepyx.py
@@ -1,5 +1,6 @@
 import moviepy.editor as mp
 import pyx.osx as osx
+from pydub import AudioSegment, silence
 
 def merge_av(audio_path, video_path, output_path):
 	output_path = osx.to_distinct(video_path) if output_path == None else output_path
@@ -14,6 +15,18 @@ def set_video_duration(video_path, duration, speed_based=False, output_path=None
 	#return video
 	#return output_path
 
+def remove_silence(video, silence_threshold=-60, min_silence_len=1500):
+	audio = video.audio
+	temp_path = osx.to_distinct('TEMP.wav')
+	audio.write_audiofile(temp_path)#, codec='pcm_s16le')
+	audio_segment = AudioSegment.from_wav(temp_path)
+	nonsilent_ranges = silence.detect_nonsilent(audio_segment, min_silence_len=min_silence_len, silence_thresh=silence_threshold)
+	os.remove(temp_path)
+	clips = []
+	for start, end in nonsilent_ranges:
+		clip = video.subclip(start / 1000.0, end / 1000.0)
+		clips.append(clip)
+	return concatenate_videoclips(clips)
 
 
 
