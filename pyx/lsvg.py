e7a9d993ea9d5b0356c7fcb6ab1819f2cd3da655
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-08-03 22:58:13 -0300

    Update lsvg.py

--- pyx/lsvg.py
+++ pyx/lsvg.py
@@ -12,3 +12,18 @@ def clip(obj, mask):
 	clip_path.append(mask)
 
 def href(image): return image.get('{http://www.w3.org/1999/xlink}href', None)
+
+def rect_attrib(rct): return { 'x': str(rct.min[0]), 'y': str(rct.min[1]), 'width': str(rct.size[0]), 'height': str(rct.size[1]) }
+
+def rect(parent, rct):
+	return etree.SubElement(parent, 'rect', **rect_attrib(rct), style="fill:none;stroke:black;stroke-width:1")
+
+class bbox:
+	def circle(cx, cy, r): return npx.rect2(cx - r, cy - r, r * 2, r * 2)
+	def ellipse(cx, cy, rx, ry): return npx.rect2(cx - rx, cy - ry, rx * 2, ry * 2)
+
+def circle_bbox(obj): return bbox.circle(*get(obj, float, 'cx', 'cy', 'r'))
+
+def ellipse_bbox(obj): return bbox.ellipse(*get(obj, float, 'cx', 'cy', 'rx', 'ry'))
+
+def rect_bbox(obj): return npx.rect2(*get(obj, float, 'x', 'y', 'width', 'height'))
