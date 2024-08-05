aae3c96670dbd4dbd13fd9a69f0ba0b5f4a6711b
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-08-05 01:51:00 -0300

    Update lsvg.py

--- pyx/lsvg.py
+++ pyx/lsvg.py
@@ -20,6 +20,9 @@ def rect_attrib(rct): return { 'x': str(rct.min[0]), 'y': str(rct.min[1]), 'widt
 def rect(parent, rct):
 	return etree.SubElement(parent, 'rect', **rect_attrib(rct), style="fill:none;stroke:black;stroke-width:1")
 
+def image(parent, rct, href):
+	return etree.SubElement(parent, '{http://www.w3.org/2000/svg}image', **rect_attrib(rct), '{http://www.w3.org/1999/xlink}href'=href)
+
 def circle_bbox(obj): return npx.bbox.circle(*get(obj, float, 'cx', 'cy', 'r'))
 
 def ellipse_bbox(obj): return npx.bbox.ellipse(*get(obj, float, 'cx', 'cy', 'rx', 'ry'))
