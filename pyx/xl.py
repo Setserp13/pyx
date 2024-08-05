32556ee697142747ebd2773e25f3642bc21e3471
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-08-05 14:23:34 -0300

    Update xl.py

--- pyx/xl.py
+++ pyx/xl.py
@@ -192,7 +192,7 @@ def wscpy(dst, src, min_row=1, min_col=1):
 			cellcpy(dst.cell(i + min_row, j + min_col), src.cell(i + 1, j + 1))
 			#dst.cell(i + min_row, j + min_col).value = src.cell(i + 1, j + 1).value
 			#stylecpy(dst.cell(i + min_row, j + min_col), src.cell(i + 1, j + 1))
-	dst.merged_cells = deepcopy(src.merged_cells)
+	dst.merged_cells = copy(src.merged_cells)
 	"""for merged_cell in src.merged_cells.ranges:
 		#print(merged_cell)
 		if ":" not in str(merged_cell): continue
