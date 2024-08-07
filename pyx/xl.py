3c759daf18776894d447fcab63adebec5aa9efbf
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-08-07 08:07:52 -0300

    Update xl.py

--- pyx/xl.py
+++ pyx/xl.py
@@ -192,11 +192,11 @@ def wscpy(dst, src, min_row=1, min_col=1):
 			cellcpy(dst.cell(i + min_row, j + min_col), src.cell(i + 1, j + 1))
 			#dst.cell(i + min_row, j + min_col).value = src.cell(i + 1, j + 1).value
 			#stylecpy(dst.cell(i + min_row, j + min_col), src.cell(i + 1, j + 1))
-	dst.merged_cells = copy(src.merged_cells)
-	"""for merged_cell in src.merged_cells.ranges:
+	#dst.merged_cells = copy(src.merged_cells)
+	for merged_cell in src.merged_cells.ranges:
 		#print(merged_cell)
 		if ":" not in str(merged_cell): continue
-		dst.merge_cells(translate_range_address(str(merged_cell), min_row - 1, min_col - 1))"""
+		dst.merge_cells(translate_range_address(str(merged_cell), min_row - 1, min_col - 1))
 	for dv in src.data_validations.dataValidation:
 		dvcopy = deepcopy(dv)
 		dvcopy.sqref = MultiCellRange()
