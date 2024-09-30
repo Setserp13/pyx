7e0562bbe3313cda4334185ac8845f90a4a5257b
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-09-29 23:34:51 -0300

    Update collectionsx.py

--- pyx/collectionsx.py
+++ pyx/collectionsx.py
@@ -1,9 +1,12 @@
 
-class Dict
+class Dict:
+	def items(dct, keys): return { x: dct[x] for x in keys if x in dct } #also works with list and tuple
 	def get(dct, key, dflt_value=None): return dct[key] if key in dct else dflt_value
 
-class List
+class List:
+	def items(arr, indices): return [arr[x] for x in indices if x in range(len(arr))] #also works with dict
 	def get(arr, index, dflt_value=None): return arr[index] if index in range(len(arr)) else dflt_value
+	
 
 #Left shift the array arr by n positions
 def left_shift(arr, n=1):
