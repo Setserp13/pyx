2c96c1a5a016a85c5069b35420c0b0663f044ee8
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-09-29 23:31:21 -0300

    Update collectionsx.py

--- pyx/collectionsx.py
+++ pyx/collectionsx.py
@@ -1,4 +1,10 @@
 
+class Dict
+	def get(dct, key, dflt_value=None): return dct[key] if key in dct else dflt_value
+
+class List
+	def get(arr, index, dflt_value=None): return arr[index] if index in range(len(arr)) else dflt_value
+
 #Left shift the array arr by n positions
 def left_shift(arr, n=1):
 	return arr[n:] + arr[:n]
