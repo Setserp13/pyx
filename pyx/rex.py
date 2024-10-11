5aa9c314dc4d4f3872ad443b06254cf51c670f85
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-10-11 12:45:04 -0300

    Update rex.py

--- pyx/rex.py
+++ pyx/rex.py
@@ -1,3 +1,13 @@
 import re
 
 def findnumbers(string): return [float(x) for x in re.findall(r'-?\d+\.?\d*', string)]
+
+def strpdict(obj, sep=[';', ':']):
+	result = {}
+	List.items = [] if obj == '' else obj.split(sep[0])
+	for item in List.items:
+		key, value = item.split(sep[1])
+		result[key.strip()] = value.strip()
+	return result
+
+def strfdict(obj, sep=[';', ':']): return sep[0].join([f'{k}{sep[1]}{obj[k]}' for k in obj])
