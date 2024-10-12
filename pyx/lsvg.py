aa626fb797f893d3554d7aff40ce9d3b2610944f
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-10-11 22:22:49 -0300

    Update lsvg.py

--- pyx/lsvg.py
+++ pyx/lsvg.py
@@ -7,9 +7,10 @@ def clip(obj, mask):
 	root = obj.getroottree().getroot()
 	clip_path_id = 'clipPath' + str(int(uuid.uuid4()))
 	obj.set('clip-path', f'url(#{clip_path_id})')
-	defs = root.find('.//{http://www.w3.org/2000/svg}defs')
+	"""defs = root.find('.//{http://www.w3.org/2000/svg}defs')
 	if defs is None:
-		defs = etree.SubElement(root, '{http://www.w3.org/2000/svg}defs')
+		defs = etree.SubElement(root, '{http://www.w3.org/2000/svg}defs')"""
+	defs = get_or_create(root, '{http://www.w3.org/2000/svg}defs')
 	clip_path = etree.SubElement(defs, '{http://www.w3.org/2000/svg}clipPath', attrib={'clipPathUnits': 'userSpaceOnUse', 'id': clip_path_id})
 	clip_path.append(mask)
 
