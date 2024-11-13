5aa4490cb61319b861341c912d058d12ded29467
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-11-13 07:52:35 -0300

    Update pandasx.py

--- pyx/pandasx.py
+++ pyx/pandasx.py
@@ -240,5 +240,6 @@ def join(objs, on): #where len(objs) > 0
 from pyx.sqlx import table_names
 
 def read_db(con, table=None):
-	table = table_names(con) if table == None
+	if table == None:
+		table = table_names(con)
 	return { x: pd.read_sql(f'SELECT * FROM {x}', con) for x in table }
