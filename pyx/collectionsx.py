09f85154a758c3d671a62179f01f8de5b4545eb8
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-11-13 10:15:26 -0300

    Update collectionsx.py

--- pyx/collectionsx.py
+++ pyx/collectionsx.py
@@ -8,10 +8,7 @@ class List:
 	def get(arr, index, dflt_value=None): return arr[index] if index in range(len(arr)) else dflt_value
 	
 
-#Left shift the array arr by n positions
-def left_shift(arr, n=1):
-	return arr[n:] + arr[:n]
 
-#Right shift the array arr by n positions
-def right_shift(arr, n=1):
-	return arr[-n:] + arr[:-n]
+def left_shift(arr, n=1): return arr[n:] + arr[:n]	#Left shift the array arr by n positions
+
+def right_shift(arr, n=1): return arr[-n:] + arr[:-n]	#Right shift the array arr by n positions
