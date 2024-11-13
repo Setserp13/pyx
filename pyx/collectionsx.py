8ad724e90089de55004716a6a596b0296f658d3f
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-11-13 10:16:37 -0300

    Update collectionsx.py

--- pyx/collectionsx.py
+++ pyx/collectionsx.py
@@ -9,6 +9,6 @@ class List:
 	
 
 
-def left_shift(arr, n=1): return arr[n:] + arr[:n]	#Left shift the array arr by n positions
+def lshift(arr, n=1): return arr[n:] + arr[:n]		#Left shift the array arr by n positions
 
-def right_shift(arr, n=1): return arr[-n:] + arr[:-n]	#Right shift the array arr by n positions
+def rshift(arr, n=1): return arr[-n:] + arr[:-n]	#Right shift the array arr by n positions
