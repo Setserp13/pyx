2124d9c3eb220c0302c7c9bf8b6c977a3a604be3
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-12-11 09:55:17 -0300

    Update xl.py

--- pyx/xl.py
+++ pyx/xl.py
@@ -487,20 +487,24 @@ def set_range_border_by_group(ws, min_row, min_col, max_row, max_col, outline=th
 		set_range_border(ws, i, min_col, area[2], max_col, outline, inline)
 		i = area[2] + 1
 
-def find_address(ws, value):
+def find_address(ws, match):
 	for i in range(1, ws.max_row + 1):
 		for j in range(1, ws.max_column + 1):
-			if value == ws.cell(i, j).value:
+			if match(ws.cell(i, j)):
 				return (i, j)
 	return None
 
-def find_addresses(ws, value):
+def find_addresses(ws, value, match = lambda a, b: return a == b):
 	result = []
 	for i in range(1, ws.max_row + 1):
 		for j in range(1, ws.max_column + 1):
-			if value == ws.cell(i, j).value:
+			if match(ws.cell(i, j)):
 				result.append((i, j))
 	return result
+
+def address_of(ws, value): return find_address(ws, lambda x: return x.value == value)
+
+def addresses_of(ws, value): return find_addresses(ws, lambda x: return x.value == value)
 
 def replace(ws, old_value, new_value, all=True):
 	for i in range(1, ws.max_row + 1):
