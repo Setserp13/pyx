c85d6cb6bced48ea7ff3fe1eb24545db4ecd5b75
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-03-31 21:05:32 -0300

    Update rex.py

--- pyx/rex.py
+++ pyx/rex.py
@@ -13,3 +13,11 @@ def strpdict(obj, sep=[';', ':']):
 	return result
 
 def strfdict(obj, sep=[';', ':']): return sep[0].join([f'{k}{sep[1]}{obj[k]}' for k in obj])
+
+def to_intervals(string):
+	labels = [rex.findnumbers(x) for x in string.split('\n')]
+	for i, x in enumerate(text.split('\n')):
+		#print(i)
+		if i < len(labels) - 1:
+			if labels[i][0] == labels[i][1]:
+				labels[i][1] = labels[i+1][0]
