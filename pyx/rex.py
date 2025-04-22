d51781a35ff0633f691996829caa963f55b3d380
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-04-21 22:18:06 -0300

    Update rex.py

--- pyx/rex.py
+++ pyx/rex.py
@@ -15,6 +15,18 @@ def strpdict(obj, sep=[';', ':']):
 def strfdict(obj, sep=[';', ':']): return sep[0].join([f'{k}{sep[1]}{obj[k]}' for k in obj])
 
 def to_label_track(string):
+	pattern = r'\s*(\S+)\s+(\S+)\s*(.*)'
+	result = [re.match(pattern, x) for x in string.split('\n')]
+	result = [list(x.groups()) for x in result if x is not None]
+	result = [[float(x[0]), float(x[1])] + x[2:] for x in result]
+	for i in range(len(result)):
+		#print(i)
+		if i < len(result) - 1:
+			if result[i][0] == result[i][1]:
+				result[i][1] = result[i+1][0]
+	return result
+
+"""def to_label_track(string):
 	result = [findnumbers(x) for x in string.split('\n')]
 	result = [x for x in result if len(x) == 2]
 	for i in range(len(result)):
@@ -22,4 +34,4 @@ def to_label_track(string):
 		if i < len(result) - 1:
 			if result[i][0] == result[i][1]:
 				result[i][1] = result[i+1][0]
-	return result
+	return result"""
