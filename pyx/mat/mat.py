8cfaed8b554a95d2b94d82410efecfe78e2a00a7
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-05-05 23:05:27 -0300

    Update mat.py

--- pyx/mat/mat.py
+++ pyx/mat/mat.py
@@ -195,21 +195,3 @@ def to_number(s, alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
 		return sum(index_of(alphabet, x) * (26 ** i) for i, x in enumerate(reversed(s)))
 	except:
 		print(f'ValueError: invalid literal: {s}')
-
-
-def bezier(t, points):
-    """Calculate a point on a Bézier curve given a parameter t."""
-    n = len(points) - 1
-    point = np.zeros(2)
-    for i in range(n + 1):
-        binomial_coeff = np.math.comb(n, i)  # Binomial coefficient
-        point += (binomial_coeff * ((1 - t) ** (n - i)) * (t ** i)) * points[i]
-    return point
-
-
-
-
-
-
-
-
