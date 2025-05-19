8067a50b9baea30adbcd6b18d87342cc9dd084cb
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-05-18 23:56:07 -0300

    Update numpyx.py

--- pyx/numpyx.py
+++ pyx/numpyx.py
@@ -160,7 +160,7 @@ class rect2(rect):
 	def bottom_right(rect): return rect.denormalize_point(np.array([0, 1]))
 	def top_left(rect): return rect.denormalize_point(np.array([1, 0]))
 	def top_right(rect): return rect.denormalize_point(np.array([1, 1]))
-	def corners(rect): return [bottom_left(rect), top_left(rect), top_right(rect), bottom_right(rect)]
+	def corners(rect): return [rect2.bottom_left(rect), rect2.top_left(rect), rect2.top_right(rect), rect2.bottom_right(rect)]
 	def area(rect): return rect.size[0] * rect.size[1]
 	def cut(rect, t, axis=0, expand=0):
 		u = axis_unit_vector(axis, 2) * t
