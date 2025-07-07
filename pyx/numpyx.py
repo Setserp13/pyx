b6d007ea983e0ae16a9b7f356c1ca105ffa1903c
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-07-06 21:25:01 -0300

    Update numpyx.py

--- pyx/numpyx.py
+++ pyx/numpyx.py
@@ -17,7 +17,7 @@ def clamp_magnitude(vector, max_magnitude):
 	current_magnitude = np.linalg.norm(vector)
 	if current_magnitude > max_magnitude:
 		vector = (vector / current_magnitude) * max_magnitude
-    	return vector
+	return vector
 
 def lerp(a, b, t): return a * (1 - t) + b * t
 
