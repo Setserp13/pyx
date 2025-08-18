ba3110b60e00ce2ab6f4d3d0e9703ee58170dc93
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-08-17 22:39:46 -0300

    Update lsvg.py

--- pyx/lsvg.py
+++ pyx/lsvg.py
@@ -14,18 +14,19 @@ def clip(obj, mask):
 	clip_path = etree.SubElement(defs, '{http://www.w3.org/2000/svg}clipPath', attrib={'clipPathUnits': 'userSpaceOnUse', 'id': clip_path_id})
 	clip_path.append(mask)
 
-def href(image): return image.get('{http://www.w3.org/1999/xlink}href', None)
+#def href(image): return image.get('{http://www.w3.org/1999/xlink}href', None)
 
-def rect_attrib(rct): return { 'x': str(rct.min[0]), 'y': str(rct.min[1]), 'width': str(rct.size[0]), 'height': str(rct.size[1]) }
+#def rect_attrib(rct): return { 'x': str(rct.min[0]), 'y': str(rct.min[1]), 'width': str(rct.size[0]), 'height': str(rct.size[1]) }
 
-def rect(parent, rct):
-	return etree.SubElement(parent, 'rect', **rect_attrib(rct), style="fill:none;stroke:black;stroke-width:1")
+#def rect(parent, rct):
+#	return etree.SubElement(parent, 'rect', **rect_attrib(rct), style="fill:none;stroke:black;stroke-width:1")
 
-def image(parent, rct, href):
-	return etree.SubElement(parent, '{http://www.w3.org/2000/svg}image', **rect_attrib(rct), **{'{http://www.w3.org/1999/xlink}href': href})
+#def image(parent, rct, href):
+#	return etree.SubElement(parent, '{http://www.w3.org/2000/svg}image', **rect_attrib(rct), **{'{http://www.w3.org/1999/xlink}href': href})
 
 def circle_bbox(obj): return npx.bbox.circle(*get(obj, float, 'cx', 'cy', 'r'))
 
 def ellipse_bbox(obj): return npx.bbox.ellipse(*get(obj, float, 'cx', 'cy', 'rx', 'ry'))
 
 def rect_bbox(obj): return npx.rect2(*get(obj, float, 'x', 'y', 'width', 'height'))
+
