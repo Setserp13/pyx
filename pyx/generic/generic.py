2bbbeb06cd64a1b70b4cd5f4380a7804aad0797c
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-09-25 22:42:05 -0300

    Update generic.py

--- pyx/generic/generic.py
+++ pyx/generic/generic.py
@@ -1,3 +1,10 @@
+def get(obj, *keys): return [getattr(obj, k) for x in keys]
+
+def set(obj, **kwargs):
+	for k in kwargs:
+		setattr(obj, k, kwargs[k])
+	return obj
+
 class Event(list):
 	def invoke(self, *args, **kwargs):
 		for x in self:
