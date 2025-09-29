23259b5cb3c7806b53a73362a3ebd1d3564ad83b
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-09-29 18:33:00 -0300

    Update numpyx.py

--- pyx/numpyx.py
+++ pyx/numpyx.py
@@ -347,6 +347,9 @@ def rotate(v, angle_rad):
 	result = rotation * complex(v[0], v[1])
 	return np.array([result.real, result.imag])
 
+def rotate_around(point, angle, center=np.zeros(2)): #use default center to rotate vectors
+	return rotate(point - center, angle) + center
+
 class bbox:
 	def circle(cx, cy, r): return rect2(cx - r, cy - r, r * 2, r * 2)
 	def ellipse(cx, cy, rx, ry): return rect2(cx - rx, cy - ry, rx * 2, ry * 2)
