62efdf7fbdfc21ba65d3b76fd3f4679c2df09753
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-10-01 20:26:07 -0300

    Update rex.py

--- pyx/rex.py
+++ pyx/rex.py
@@ -1,6 +1,38 @@
 import re
 from pyx.collectionsx import List
 
+def split_at(lst, *indices):
+	if len(indices) == 0:
+		return [lst]
+	result = [lst[:indices[0]]]
+	for i in range(1, len(indices)):
+		result.append(lst[indices[i-1]:indices[i]])
+	result.append(lst[indices[-1]:])
+	return result
+
+#RANGE NOTATION
+
+def collapse(lst):	#Returns a string
+	lst = sorted(lst)
+	indices = [i for i in range(1, len(lst)) if lst[i] - lst[i-1] > 1]
+	result = split_at(lst, *indices)
+	result = [f'{x[0]}-{x[-1]}' if len(x) > 1 else str(x[0]) for x in result]
+	return ','.join(result)
+
+def expand(s):  # Returns a list of ints
+	s = s.replace(' ', '')
+	result = []
+	for x in s.split(','):
+		y = x.split('-')
+		if len(y) == 1:
+			result.append(int(y[0]))
+		else:
+			start, end = int(y[0]), int(y[1])
+			result.extend(range(start, end + 1))
+	return result
+
+#RANGE NOTATION
+
 #def findnumbers(string): return [float(x) for x in re.findall(r'-?\d+\.?\d*', string)]
 def findnumbers(string): return [float(x) if '.' in x else int(x) for x in re.findall(r'-?\d+\.?\d*', string)]
 
