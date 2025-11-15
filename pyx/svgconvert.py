c5caf7051cce605c0bb79d31bef1ccb39ad37c26
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-11-15 14:56:23 -0300

    Update svgconvert.py

--- pyx/svgconvert.py
+++ pyx/svgconvert.py
@@ -4,7 +4,24 @@ import cairosvg
 from lxml import etree
 import pyx.svgx as svgx
 
-def export_group_to_png(element, output_png, dpi=10):	#96):
+def svg_to_png(tree, output_png, rect, dpi=10):
+    root = tree.getroot()	
+    #print(rect.to_tuple())
+    if rect.size[0] < 1 or rect.size[1] < 1: return #'CUZ NO SIZE CAN BE LESSER THAN 1
+
+    # Modify the SVG content to include a viewBox attribute
+    root.attrib['viewBox'] = f"{rect.min[0]} {rect.min[1]} {rect.size[0]} {rect.size[1]}"
+
+    # Create an in-memory file-like object
+    svg_buffer = BytesIO()
+    tree.write(svg_buffer, encoding='utf-8', xml_declaration=True)
+
+    # Use the in-memory SVG content directly, no need for a temporary file
+    svg_buffer.seek(0)
+
+    cairosvg.svg2png(file_obj=svg_buffer, write_to=output_png, output_width=rect.size[0], output_height=rect.size[1], dpi=dpi)
+
+def element_to_png(element, output_png, dpi=10):	#96):
 	bbox = svgx.bbox(element)
 	if not bbox:
 		return
