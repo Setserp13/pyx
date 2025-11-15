3fbb7109d78c2e4459c7bbadcc511ab46ddd62bf
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-11-15 15:54:50 -0300

    Update svgconvert.py

--- pyx/svgconvert.py
+++ pyx/svgconvert.py
@@ -28,7 +28,7 @@ def element_to_png(element, output_png, dpi=10):	#96):
 	if not bbox:
 		return
 
-	new_svg = svgx.svg(0, 0)	# ========== BUILD A NEW SVG ==========
+	new_svg = svgx.svg(100, 100)	# ========== BUILD A NEW SVG ==========
 	new_svg.append(element)
 
 	new_svg.attrib["viewBox"] = f"{bbox.min[0]} {bbox.min[1]} {bbox.size[0]} {bbox.size[1]}"
