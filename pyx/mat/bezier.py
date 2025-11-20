0b0c4e8e49803ce691799ad635df7a347e4253f8
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-11-20 11:08:48 -0300

    Update bezier.py

--- pyx/mat/bezier.py
+++ pyx/mat/bezier.py
@@ -3,34 +3,12 @@ import numpy as np
 import pyx.numpyx as npx
 
 class bezier(np.ndarray):
-	def __new__(cls, input_array, endpoints=None):
-		# Convert input to ndarray and view it as MyArray
+	def __new__(cls, input_array):
 		obj = np.asarray(input_array).view(cls)
-		#obj.endpoints = [True] * len(input_array) if endpoints is None else endpoints
-		obj.endpoints = list(range(len(input_array))) if endpoints is None else endpoints
 		return obj
 
 	def __array_finalize__(self, obj):
-		# Called when the object is created via view/slicing
 		if obj is None: return
-		# You can set custom attributes here if needed
-		self.my_attribute = getattr(obj, 'my_attribute', 'default')
-
-	def path(self):
-		result = []
-		result.append(self[self.endpoints[0]:self.endpoints[0]+1])
-		for i in range(len(self.endpoints) - 1):
-			result.append(self[self.endpoints[i]+1:self.endpoints[i+1]+1])
-		result.append(np.concatenate([self[self.endpoints[-1]+1:], self[:self.endpoints[0]+1]]))
-		return result
-
-	def d(self):
-		d = ''
-		for x in self.path():
-			d += 'MQC'[len(x) - 1]
-			d += ' ' + ' '.join([str(y) for y in x.flatten()]) + ' '
-		#print(d)
-		return d
 
 	def get_point(p, t):
 		# Recursive Bézier evaluation (De Casteljau's algorithm)
@@ -55,6 +33,39 @@ class bezier(np.ndarray):
 			raise ValueError("Need at least two points for a derivative")
 		derivative_points = [n * (p[i + 1] - p[i]) for i in range(n)]
 		return get_point(t, *derivative_points)
+
+
+class path(np.ndarray):	#composite Bézier curve or Bézier spline
+	def __new__(cls, input_array, endpoints=None):
+		# Convert input to ndarray and view it as MyArray
+		obj = np.asarray(input_array).view(cls)
+		#obj.endpoints = [True] * len(input_array) if endpoints is None else endpoints
+		obj.endpoints = list(range(len(input_array))) if endpoints is None else endpoints
+		return obj
+
+	def __array_finalize__(self, obj):
+		# Called when the object is created via view/slicing
+		if obj is None: return
+		# You can set custom attributes here if needed
+		self.my_attribute = getattr(obj, 'my_attribute', 'default')
+
+	def curves(self):
+		result = []
+		result.append(self[self.endpoints[0]:self.endpoints[0]+1])
+		for i in range(len(self.endpoints) - 1):
+			result.append(self[self.endpoints[i]+1:self.endpoints[i+1]+1])
+		result.append(np.concatenate([self[self.endpoints[-1]+1:], self[:self.endpoints[0]+1]]))
+		return result
+
+	def d(self):
+		d = ''
+		for x in self.curves():
+			d += 'MQC'[len(x) - 1]
+			d += ' ' + ' '.join([str(y) for y in x.flatten()]) + ' '
+		#print(d)
+		return d
+
+
 
 
 def smooth_control_points(p, index, extents=10):
