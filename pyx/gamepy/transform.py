95b2ee37c7b60cad482a656787d9d093f8fafd49
Diogo <86491085+Setserp13@users.noreply.github.com>
2025-12-07 10:33:04 -0300

    Update transform.py

--- pyx/gamepy/transform.py
+++ pyx/gamepy/transform.py
@@ -181,7 +181,8 @@ class Node2D(Node):
 
 	def TRS(self):
 		#print(self.local_TRS())
-		return functools.reduce(lambda acc, x: acc @ x, [x.local_TRS() for x in reversed([self] + self.ancestors())])
+		return functools.reduce(lambda acc, x: acc @ x.local_TRS(), reversed([self] + self.ancestors()))
+		#return functools.reduce(lambda acc, x: acc @ x, [x.local_TRS() for x in reversed([self] + self.ancestors())])
 
 	def inverse_TRS(self):
 		return functools.reduce(lambda acc, x: acc @ x, [x.local_inverse_TRS() for x in reversed([self] + self.ancestors())])
