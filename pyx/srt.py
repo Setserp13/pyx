7d25fcb409adc1b5c0e690b8c18a6b5773ec51cc
Diogo <86491085+Setserp13@users.noreply.github.com>
2026-01-11 17:09:18 -0300

    Update srt.py

--- pyx/srt.py
+++ pyx/srt.py
@@ -1,8 +1,9 @@
 import copy
-from datetime import datetime, time, timedelta
+from pyx.timeline import Time, TimeRange, Track
 import textwrap
 import pyx.mat.mat as mat
 import pyx.osx as osx
+import pyx.rex as rex
 
 def wrap_text(text, width=42, len_func=len, join=True):
 	lines = []
@@ -58,77 +59,6 @@ def srtftime(x): return x.strftime("%H:%M:%S,%f")[:-3]
 
 def srtptime(x): return datetime.strptime(x, "%H:%M:%S,%f").time()
 
-def microseconds(x): return x.microsecond + 1_000_000 * (x.second + 60 * x.minute + 3600 * x.hour)
-
-def seconds(x): return microseconds(x) / 1_000_000
-
-class Time(float):
-	"""def __new__(cls, hours=0, minutes=0, seconds=0, milliseconds=0):
-		total_seconds = hours * 3600 + minutes * 60 + seconds + milliseconds / 1000
-		return super().__new__(cls, total_seconds)"""
-	
-	def from_time(hour=0, minute=0, second=0, millisecond=0, microsecond=0):
-		total_seconds = hour * 3600 + minute * 60 + second + millisecond / 1000 + microsecond / 1_000_000
-		return Time(total_seconds)
-	
-	@property
-	def hour(self): return int(self) // 3600
-
-	@property
-	def minute(self): return (int(self) % 3600) // 60
-
-	@property
-	def second(self): return int(self) % 60
-
-	@property
-	def millisecond(self): return int((self - int(self)) * 1000)
-
-	@property
-	def microsecond(self): return int((self - int(self)) * 1_000_000)
-		
-	@property
-	def time(self): return time(hour=self.hour, minute=self.minute, second=self.second, microsecond=self.microsecond)
-
-	#def __repr__(self):
-	#	return f"Time({self.hours:02}:{self.minutes:02}:{self.seconds:02}.{self.milliseconds:03})"
-
-class TimeRange():
-	def __init__(self, start, duration):
-		self.start = Time(start)
-		self.duration = Time(duration)
-	"""def __init__(self, start, end):
-		self.start = Time(start)
-		self.duration = Time(end - start)
-		#self.end = Time(end)"""
-
-	@classmethod
-	def start_end(cls, start, end): return cls(start, end - start)
-	
-	@property
-	def end(self): return Time(self.start + self.duration)
-
-	@end.setter
-	def end(self, value): self.duration = Time(value - self.start)
-	#@property
-	#def duration(self): return Time(self.end - self.start)
-
-	def shift(self, **kwargs):	#hours, minutes, seconds, microseconds
-		self.start += timedelta(**kwargs)
-		self.end += timedelta(**kwargs)
-
-	def expand(self, **kwargs):
-		self.start -= timedelta(**kwargs)
-		self.end += timedelta(**kwargs)
-
-class Track(list):	#elements are TimeRange-like
-	def shift(self, **kwargs):
-		for x in self:
-			x.shift(**kwargs)
-
-	def expand(self, **kwargs):
-		for x in self:
-			x.expand(**kwargs)
-
 
 class SubRipItem(TimeRange): #herde essa classe de uma classe mais genérica depois, tornando-a irmã de clipes de vídeo, imagens, audios, transições, efeitos e etc...
 	def __init__(self, index, start, end, text):
@@ -179,7 +109,7 @@ class SubRipFile(Track):
 
 #print(SubRipFile.strp(osx.read('subs.srt')).strf())
 
-import pyx.rex as rex
+
 
 def create_subs(text, labels, start=0): #Text and labels must have the same number of lines. Start must be in seconds.
 	result = SubRipFile()
@@ -190,6 +120,7 @@ def create_subs(text, labels, start=0): #Text and labels must have the same numb
 		#result.append(SubRipItem(index=i+1, start=Time(milliseconds=int((start+labels[i][0])*1000)), end=Time(milliseconds=int((start+labels[i][1])*1000)), text=x))
 	return result
 	
+
 
 
 
