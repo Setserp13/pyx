a635fa16cdf5401943cc3f8f375bc9b85400c2d9
Diogo <86491085+Setserp13@users.noreply.github.com>
2026-01-20 21:09:09 -0300

    Update bezier.py

--- pyx/mat/bezier.py
+++ pyx/mat/bezier.py
@@ -109,6 +109,7 @@ class path(np.ndarray):	#composite Bézier curve or Bézier spline
 	def aabb(self, value): self[:] = npx.set_aabb(self, value)
 
 	def __rmatmul__(self, M): return path(npx.affine_transform(M, self), endpoints=self.endpoints, closed=self.closed)
+	def __matmul__(self, M): return path(npx.affine_transform(M, self), endpoints=self.endpoints, closed=self.closed)
 
 def smooth_control_points(p, index, extents=10):
 	angle = p.vertex_angle(index)
