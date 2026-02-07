d42c3d534f6c8e7899a700655a27ab4e9ebdd925
Diogo <86491085+Setserp13@users.noreply.github.com>
2026-02-07 18:56:32 -0300

    Update svgconvert.py

--- pyx/svgconvert.py
+++ pyx/svgconvert.py
@@ -7,7 +7,7 @@ import pyx.svgx as svgx
 #Convert from vector image to raster image
 
 def svg_to_png(tree, output_png, rect=None, dpi=10):
-	root = tree.getroot()
+	root = tree	#.getroot()
 	if not rect is None:
 		if rect.size[0] < 1 or rect.size[1] < 1: return #'CUZ NO SIZE CAN BE LESSER THAN 1
 		root.attrib['viewBox'] = f"{rect.min[0]} {rect.min[1]} {rect.size[0]} {rect.size[1]}"	# Modify the SVG content to include a viewBox attribute
