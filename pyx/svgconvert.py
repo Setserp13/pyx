b303c35cf9485e17c58cd6563a45f1543beae677
Diogo <86491085+Setserp13@users.noreply.github.com>
2026-02-07 18:54:54 -0300

    Update svgconvert.py

--- pyx/svgconvert.py
+++ pyx/svgconvert.py
@@ -7,22 +7,14 @@ import pyx.svgx as svgx
 #Convert from vector image to raster image
 
 def svg_to_png(tree, output_png, rect=None, dpi=10):
-    root = tree.getroot()
-    
+	root = tree.getroot()
 	if not rect is None:
-    	if rect.size[0] < 1 or rect.size[1] < 1: return #'CUZ NO SIZE CAN BE LESSER THAN 1
-
-    	# Modify the SVG content to include a viewBox attribute
-    	root.attrib['viewBox'] = f"{rect.min[0]} {rect.min[1]} {rect.size[0]} {rect.size[1]}"
-
-    # Create an in-memory file-like object
-    svg_buffer = BytesIO()
-    tree.write(svg_buffer, encoding='utf-8', xml_declaration=True)
-
-    # Use the in-memory SVG content directly, no need for a temporary file
-    svg_buffer.seek(0)
-
-    cairosvg.svg2png(file_obj=svg_buffer, write_to=output_png, output_width=rect.size[0], output_height=rect.size[1], dpi=dpi)
+		if rect.size[0] < 1 or rect.size[1] < 1: return #'CUZ NO SIZE CAN BE LESSER THAN 1
+		root.attrib['viewBox'] = f"{rect.min[0]} {rect.min[1]} {rect.size[0]} {rect.size[1]}"	# Modify the SVG content to include a viewBox attribute
+	svg_buffer = BytesIO()		# Create an in-memory file-like object
+	tree.write(svg_buffer, encoding='utf-8', xml_declaration=True)
+	svg_buffer.seek(0)	# Use the in-memory SVG content directly, no need for a temporary file
+	cairosvg.svg2png(file_obj=svg_buffer, write_to=output_png, output_width=rect.size[0], output_height=rect.size[1], dpi=dpi)
 
 def element_to_png(element, output_png, dpi=10):	#96):
 	bbox = svgx.bbox(element)
