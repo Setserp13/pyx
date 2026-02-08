aea5dbd778a51e531666ebdfb52ea09653168a84
Diogo <86491085+Setserp13@users.noreply.github.com>
2026-02-08 10:52:17 -0300

    Update numpyx_geo.py

--- pyx/numpyx_geo.py
+++ pyx/numpyx_geo.py
@@ -245,7 +245,7 @@ def add_symmetrical_handles(vertices, handle_length=.1, closed=True):
 def truncate(vertices, t=.25, closed=True):
 	result = []
 	for x in polyline.edges(vertices, closed=closed):
-		result += [npx.lerp(x[0], x[1], t), npx.lerp(x[0], x[1], 1-t)]
+		result += [npx.lerp(x[0], x[1], float(t)), npx.lerp(x[0], x[1], 1-float(t))]
 	if not closed:
 		result[0] = vertices[0]
 		result[-1] = vertices[-1]
@@ -951,6 +951,7 @@ def rects(offset, sizes, axis=0, align=0.5, gap=0.0):
 	#print(offset)
 	distribute(result, axis=axis, align=align, gap=gap)
 	return result
+
 
 
 
