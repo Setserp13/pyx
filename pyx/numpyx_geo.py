b1c97cea14ecdc86c03305943776c59845a0c967
Diogo <86491085+Setserp13@users.noreply.github.com>
2026-04-26 13:31:29 -0300

    Update numpyx_geo.py

--- pyx/numpyx_geo.py
+++ pyx/numpyx_geo.py
@@ -702,6 +702,25 @@ class arc(circle):
 		
 		return aabb(points)
 
+	@aabb.setter
+	def aabb(self, value):
+		cur = self.aabb
+	
+		cur_min, cur_max = cur
+		new_min, new_max = value
+	
+		cur_size = cur_max - cur_min
+		new_size = new_max - new_min
+	
+		scale = new_size / cur_size
+	
+		# arcs must remain circular → use uniform scale
+		s = min(scale)
+	
+		self.center = (self.center - cur_min) * s + new_min
+		self.radius *= s
+
+
 class line(points):	#start = self[0], end = self[1]
 	
 	@property
