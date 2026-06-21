5b4a9d2763dd4e1c143113b0ef6571a0bd65bd3c
Diogo <86491085+Setserp13@users.noreply.github.com>
2026-06-21 20:11:01 -0300

    Update svgx.py

--- pyx/svgx.py
+++ pyx/svgx.py
@@ -75,7 +75,7 @@ def polybezier_from_svg(obj):
 			v = [np.array([pos[0], x]) for x in args]
 		elif cmd in 'Zz':
 			closed = True
-			v.append(np.array(points[0]))
+			#v.append(np.array(points[0]))
 
 		v = geo.polyline(v)
 		k = 2 if cmd.upper() in 'QT' else 3 if cmd.upper() in 'CS' else 1
