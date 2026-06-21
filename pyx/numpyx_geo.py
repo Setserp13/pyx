061222ffdd6823bed89a6dedba5cc4bc03126560
Diogo <86491085+Setserp13@users.noreply.github.com>
2026-06-20 23:55:34 -0300

    Update numpyx_geo.py

--- pyx/numpyx_geo.py
+++ pyx/numpyx_geo.py
@@ -2188,7 +2188,7 @@ import pyx.generic.generic as generic
 
 
 shapes = [points, polybezier, circle, ellipse, line, polyline, rect, group, arc, Transform]
-for x in shapes: generic.make_Entity(x)
+for x in shapes: generic.make_Element(x)
 """	x.set = lambda self, **attrib: generic.set(self, attrib={**getattr(self, "attrib", {}), **attrib})
 	x.get = lambda self, *keys: [self.attrib[k] for k in keys] if hasattr(self, 'attrib') else None"""
 
